e71f0a7f1f79b4df6b0970a32d54d40a8042bf13
Roy Duineveld <royduineveld@gmail.com>
2020-12-16 12:11:54 +0100

    Edit account

--- resources/views/account/edit.blade.php
+++ resources/views/account/edit.blade.php
@@ -4,6 +4,44 @@
 
 @section('account-content')
     <div class="container mx-auto">
-        TODO
+        <graphql v-cloak query="{ customer { firstname lastname } }">
+            <div v-if="data" slot-scope="{ data }">
+                <graphql-mutation query="mutation { updateCustomerV2 ( input: changes ) { customer { firstname lastname } } }" :changes="data.customer" :refresh-user-info="true">
+                    <form class="sm:w-1/3 md:w-1/4" slot-scope="{ changes, mutate, mutated }" v-on:submit.prevent="mutate">
+                        <label class="font-semibold" for="firstname">@lang('Firstname')</label>
+                        <input
+                            class="form-input w-full mb-2"
+                            type="text"
+                            id="firstname"
+                            v-model="changes.firstname"
+                            placeholder="@lang('Firstname')"
+                        >
+
+                        <label class="font-semibold" for="lastname">@lang('Lastname')</label>
+                        <input
+                            class="form-input w-full"
+                            type="text"
+                            id="lastname"
+                            v-model="changes.lastname"
+                            placeholder="@lang('Lastname')"
+                        >
+
+                        <div class="flex items-center mt-5">
+                            <button
+                                type="submit"
+                                class="btn btn-primary"
+                                :disabled="$root.loading"
+                            >
+                                @lang('Change')
+                            </button>
+
+                            <div v-if="mutated" class="ml-3 text-green-500">
+                                @lang('Changed successfully!')
+                            </div>
+                        </div>
+                    </form>
+                <graphql-mutation>
+            </div>
+        </graphql>
     </div>
 @endsection

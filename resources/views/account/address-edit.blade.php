562ae855cd970f0d7098be6e67f28cb29d7433ab
Roy Duineveld <royduineveld@gmail.com>
2020-12-23 09:37:59 +0100

    Redirect

--- resources/views/account/address-edit.blade.php
+++ resources/views/account/address-edit.blade.php
@@ -6,7 +6,7 @@
     <div class="container mx-auto">
         <graphql v-cloak query="{ customer { addresses { id firstname lastname street city postcode country_code telephone company default_billing default_shipping } } }" check="data.customer.addresses.find(a => a.id == {{ request()->id }})" redirect="/account/orders">
             <div v-if="data" slot-scope="{ data }" :set="address = data.customer.addresses.find(a => a.id == {{ request()->id }})">
-                <graphql-mutation query="mutation { updateCustomerAddress ( id: {{ request()->id }}, input: changes ) { id } }" :changes="address">
+                <graphql-mutation query="mutation { updateCustomerAddress ( id: {{ request()->id }}, input: changes ) { id } }" :changes="address" redirect="/account/addresses">
                     @include('rapidez::account.partials.address-form')
                 <graphql-mutation>
             </div>

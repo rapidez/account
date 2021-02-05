29ce628e932066462b2488a2b34d2d9670b45cc5
Bob Wezelman <bob.wezelman@hotmail.com>
2021-02-05 13:24:09 +0100

    Added register form

--- resources/views/account/register.blade.php
+++ resources/views/account/register.blade.php
@@ -4,35 +4,40 @@
 
 @section('content')
     <div class="flex justify-center">
-        <graphql-mutation query="mutation { createCustomerV2 ( input: changes ) { customer { firstname lastname email } } }">
-            <form class="sm:w-1/3 md:w-1/4" slot-scope="{ changes, mutate, mutated }" v-on:submit.prevent="(changes.password !== passwordConfirm) ? alert('Passwords don\'t match') : mutate()">
+        <graphql-mutation before="checkPassword" query="mutation { createCustomerV2 ( input: changes ) { customer { firstname lastname email } } }">
+            <form class="sm:w-1/3 md:w-1/4" slot-scope="{ changes, beforeParams, mutate, mutated }" v-on:submit.prevent="mutate">
                 <h1 class="font-bold text-4xl text-center mb-5">@lang('Register account')</h1>
                 <x-rapidez::input
                     name="firstname"
+                    label="First name"
                     v-model="changes.firstname"
                     required
                 />
                 <x-rapidez::input
                     name="lastname"
+                    label="Last name"
                     v-model="changes.lastname"
                     required
                 />
                 <x-rapidez::input
                     name="email"
+                    label="E-mail"
                     type="email"
                     v-model="changes.email"
                     required
                 />
                 <x-rapidez::input
                     name="password"
+                    label="Password"
                     type="password"
                     v-model="changes.password"
                     required
                 />
                 <x-rapidez::input
                     name="passwordConfirm"
+                    label="Confirm password"
                     type="password"
-                    v-model="passwordConfirm"
+                    v-model="beforeParams.passwordConfirm"
                     required
                 />
                 <div class="flex items-center mt-5">

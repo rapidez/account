5f52cc95e40b9e97b687eb685ca88641aa69177c
Roy Duineveld <royduineveld@gmail.com>
2021-08-12 11:06:53 +0200

    Reorder functionality

--- resources/js/callbacks.js
+++ resources/js/callbacks.js
@@ -1,10 +1,21 @@
 import InteractWithUser from 'Vendor/rapidez/core/resources/js/components/User/mixins/InteractWithUser'
+import GetCart from 'Vendor/rapidez/core/resources/js/components/Cart/mixins/GetCart'
 
-Vue.prototype.registerCallback = async function (changes) {
+Vue.prototype.registerCallback = async function (changes, response) {
     await InteractWithUser.methods.login(changes.email, changes.password)
     await InteractWithUser.methods.refreshUser()
 }
 
-Vue.prototype.refreshUserInfoCallback = async function (changes) {
+Vue.prototype.refreshUserInfoCallback = async function (changes, response) {
     await InteractWithUser.methods.refreshUser()
+}
+
+Vue.prototype.reorderCallback = async function (changes, response) {
+    response.data.data.reorderItems.userInputErrors.forEach((error) => {
+        setTimeout(() => {
+            Notify(error.message, 'warning')
+        }, 1500)
+    })
+
+    await GetCart.methods.refreshCart()
 }
--- resources/views/account/partials/orders.blade.php
+++ resources/views/account/partials/orders.blade.php
@@ -7,6 +7,7 @@
                 <th class="px-4">@lang('Ship to')</th>
                 <th class="px-4">@lang('Order total')</th>
                 <th class="px-4">@lang('Status')</th>
+                <th></th>
             </tr>
         </thead>
         <tbody>
@@ -20,6 +21,15 @@
                 <td class="border px-4 py-2">@{{ order.shipping_address?.firstname }} @{{ order.shipping_address?.lastname }}</td>
                 <td class="border px-4 py-2">@{{ order.total.grand_total.value | price }}</td>
                 <td class="border px-4 py-2">@{{ order.status }}</td>
+                <td class="border px-4 py-2">
+                    <graphql-mutation :query='"mutation { reorderItems(orderNumber: \""+order.number+"\") { cart { id } userInputErrors { message } } }"' redirect="/cart" :callback="reorderCallback">
+                        <form slot-scope="{ mutate }" v-on:submit.prevent="mutate">
+                            <x-rapidez::button type="submit" class="py-1 px-2">
+                                @lang('Reorder')
+                            </x-rapidez::button>
+                        </form>
+                    </graphql-mutation>
+                </td>
             </tr>
         </tbody>
     </table>

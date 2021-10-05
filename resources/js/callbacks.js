00aff0510935f520ac92e703c6bc1927dc3f5c16
Bob Wezelman <bob.wezelman@hotmail.com>
2021-10-05 11:24:35 +0200

    Add variables to callbacks (#10)

--- resources/js/callbacks.js
+++ resources/js/callbacks.js
@@ -1,16 +1,16 @@
 import InteractWithUser from 'Vendor/rapidez/core/resources/js/components/User/mixins/InteractWithUser'
 import GetCart from 'Vendor/rapidez/core/resources/js/components/Cart/mixins/GetCart'
 
-Vue.prototype.registerCallback = async function (changes, response) {
+Vue.prototype.registerCallback = async function (changes, variables, response) {
     await InteractWithUser.methods.login(changes.email, changes.password)
     await InteractWithUser.methods.refreshUser()
 }
 
-Vue.prototype.refreshUserInfoCallback = async function (changes, response) {
+Vue.prototype.refreshUserInfoCallback = async function (changes, variables, response) {
     await InteractWithUser.methods.refreshUser()
 }
 
-Vue.prototype.reorderCallback = async function (changes, response) {
+Vue.prototype.reorderCallback = async function (changes, variables, response) {
     response.data.data.reorderItems.userInputErrors.forEach((error) => {
         setTimeout(() => {
             Notify(error.message, 'warning')

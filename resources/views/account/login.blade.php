3b662c81df491d71d517c26b3ecfcbca9656823c
Roy Duineveld <royduineveld@gmail.com>
2022-01-12 12:14:11 +0100

    Show login form directly when not logged in

--- resources/views/account/login.blade.php
+++ resources/views/account/login.blade.php
@@ -3,36 +3,5 @@
 @section('title', __('Login'))
 
 @section('content')
-    <login v-cloak :checkout-login="false" v-slot="{ email, password, go, loginInputChange }">
-        <div v-if="!$root.user" class="flex justify-center">
-            <form class="p-8 border rounded w-[400px]" v-on:submit.prevent="go()">
-                <h1 class="font-bold text-4xl text-center mb-5">@lang('Login')</h1>
-
-                <x-rapidez::input
-                    :label="false"
-                    name="email"
-                    type="email"
-                    v-bind:value="email"
-                    v-on:input="loginInputChange"
-                    required
-                />
-                <x-rapidez::input
-                    :label="false"
-                    class="mt-3"
-                    name="password"
-                    type="password"
-                    v-bind:value="password"
-                    v-on:input="loginInputChange"
-                    required
-                />
-
-                <x-rapidez::button type="submit" class="w-full my-5" dusk="continue">@lang('Login')</x-rapidez::button>
-
-                <div class="flex justify-between">
-                    <a href="/register" class="text-sm hover:underline">@lang('Create an account')</a>
-                    <a href="/forgotpassword" class="text-sm hover:underline">@lang('Forgot your password?')</a>
-                </div>
-            </form>
-        </div>
-    </login>
+    @include('rapidez::account.partials.login')
 @endsection
--- resources/views/account/partials/layout.blade.php
+++ resources/views/account/partials/layout.blade.php
@@ -3,9 +3,14 @@
 @section('title', __('Account'))
 
 @section('content')
-    <div class="container mx-auto">
-        @include('rapidez::account.partials.menu')
-        <h1 class="font-bold text-4xl my-3">@yield('title')</h1>
-        @yield('account-content')
+    <div class="container mx-auto" v-cloak>
+        <div v-if="$root.user">
+            @include('rapidez::account.partials.menu')
+            <h1 class="font-bold text-4xl my-3">@yield('title')</h1>
+            @yield('account-content')
+        </div>
+        <div v-else>
+            @include('rapidez::account.partials.login', ['redirect' => false])
+        </div>
     </div>
 @endsection

60477160cf25e283add1ea02b241ddf8488326a3
Roy Duineveld <royduineveld@gmail.com>
2022-01-12 11:49:05 +0100

    Show the newest orders first on the overview

--- resources/views/account/overview.blade.php
+++ resources/views/account/overview.blade.php
@@ -4,7 +4,7 @@
 
 @section('account-content')
     <div class="container mx-auto">
-        <graphql v-cloak query="@include('rapidez::account.partials.queries.overview')">
+        <graphql v-cloak query="@include('rapidez::account.partials.queries.overview')" :callback="sortOrdersCallback">
             <div v-if="data" slot-scope="{ data }">
                 <h2 class="font-bold text-2xl mt-5 mb-3">@lang('Account information')</h2>
                 <div class="text-gray-700">

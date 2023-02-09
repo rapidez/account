d6274bfed054a7ca2800ac894c3aae894a886fca
Roene <roene@justbetter.nl>
2023-02-09 15:13:48 +0100

    JFE-672 Changed styling login/register/forgot password form

--- resources/views/account/forgotpassword.blade.php
+++ resources/views/account/forgotpassword.blade.php
@@ -12,12 +12,12 @@
         :notify="{ message: '@lang('An email is send with a password reset link if an account exists with the provided email address.')' }"
     >
         <div
-            class="flex flex-col items-center"
+            class="flex flex-col items-center bg-highlight"
             slot-scope="{ mutate, variables }"
         >
             <h1 class="my-5 text-3xl font-bold text-gray-700">@lang('Forgot Your Password?')</h1>
             <form
-                class="flex w-[400px] flex-col gap-3 rounded border bg-white p-8 shadow"
+                class="flex w-[500px] flex-col gap-3 p-8"
                 v-on:submit.prevent="mutate"
             >
                 <x-rapidez::input

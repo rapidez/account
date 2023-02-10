6a50155067afb73b3ea65b1deab4966e8fa091de
Roene <roene@justbetter.nl>
2023-02-10 15:54:19 +0100

    Fixed grid layout for mobile

--- resources/views/account/partials/address-form.blade.php
+++ resources/views/account/partials/address-form.blade.php
@@ -1,56 +1,56 @@
 <form slot-scope="{ variables, mutate, mutated }" v-on:submit.prevent="mutate">
-    <div class="grid grid-cols-6 gap-4">
-        <h2 class="col-span-6 font-bold text-2xl">@lang('Contact information')</h2>
-        <div class="col-span-6 sm:col-span-3">
+    <div class="grid grid-cols-12 gap-4">
+        <h2 class="col-span-12 font-bold text-2xl">@lang('Contact information')</h2>
+        <div class="col-span-12 sm:col-span-6">
             <x-rapidez::input name="firstname" v-model="variables.firstname" :label="__('Firstname')"  required />
         </div>
         @if(Rapidez::config('customer/address/middlename_show', 0))
-            <div class="col-span-6 sm:col-span-3">
+            <div class="col-span-12 sm:col-span-6">
                 <x-rapidez::input name="middlename" v-model="variables.middlename" :label="__('Middlename')" />
             </div>
         @endif
-        <div class="col-span-6 sm:col-span-3">
+        <div class="col-span-12 sm:col-span-6">
             <x-rapidez::input name="lastname" v-model="variables.lastname" :label="__('Lastname')"  required />
         </div>
 
-        <div class="col-span-6 sm:col-span-3">
+        <div class="col-span-12 sm:col-span-6">
             @if(Rapidez::config('customer/address/company_show', 'opt'))
                 <x-rapidez::input name="company" v-model="variables.company" :label="__('Company')" :required="Rapidez::config('customer/address/company_show', 'opt') == 'req'" />
             @endif
         </div>
 
-        <div class="col-span-6 sm:col-span-3">
+        <div class="col-span-12 sm:col-span-6">
             @if(Rapidez::config('customer/address/telephone_show', 'req'))
                 <x-rapidez::input name="telephone" v-model="variables.telephone" :label="__('Telephone')" :required="Rapidez::config('customer/address/telephone_show', 'req') == 'req'" />
             @endif
         </div>
 
-        <h2 class="col-span-6 font-bold text-2xl mt-2">@lang('Address')</h2>
-        <div class="col-span-6 sm:col-span-2">
+        <h2 class="col-span-12 font-bold text-2xl mt-2">@lang('Address')</h2>
+        <div class="col-span-12 sm:col-span-4">
             <x-rapidez::input name="street[0]" v-model="variables.street[0]" :label="__('Street')" :placeholder="__('Street')" required />
         </div>
-        <div class="col-span-6 sm:col-span-2">
+        <div class="col-span-12 sm:col-span-4">
             @if(Rapidez::config('customer/address/street_lines', 2) >= 2)
                 <x-rapidez::input name="street[1]" v-model="variables.street[1]" :label="__('Housenumber')" :placeholder="__('Nr.')" />
             @endif
         </div>
-        <div class="col-span-6 sm:col-span-2">
+        <div class="col-span-12 sm:col-span-4">
             @if(Rapidez::config('customer/address/street_lines', 2) >= 3)
                 <x-rapidez::input name="street[2]" v-model="variables.street[2]" :label="__('Addition')" :placeholder="__('Nr.')" />
             @endif
         </div>
-        <div class="col-span-12 sm:col-span-6">
-            @if(Rapidez::config('customer/address/street_lines', 2) >= 4)
-                <x-rapidez::input name="street[3]" v-model="variables.street[3]" label="" placeholder="" />
-            @endif
-        </div>
-        <div class="col-span-6 sm:col-span-2">
+        @if(Rapidez::config('customer/address/street_lines', 2) >= 4)
+            <div class="col-span-12 sm:col-span-4">
+                    <x-rapidez::input name="street[3]" v-model="variables.street[3]" label="" placeholder="" />
+            </div>
+        @endif
+        <div class="col-span-12 sm:col-span-4">
             <x-rapidez::input name="postcode" v-model="variables.postcode" :label="__('Postcode')" required />
         </div>
-        <div class="col-span-6 sm:col-span-2">
+        <div class="col-span-12 sm:col-span-4">
             <x-rapidez::input name="city" v-model="variables.city" :label="__('City')" required />
         </div>
-        <div class="col-span-6 sm:col-span-2">
+        <div class="col-span-12 sm:col-span-4">
             <x-rapidez::country-select
                 name="country_code"
                 :label="__('Country')"
@@ -59,10 +59,10 @@
                 required
             />
         </div>
-        <div class="col-span-6">
+        <div class="col-span-12">
             <x-rapidez::checkbox v-model="variables.default_billing">@lang('Default billing address')</x-rapidez::checkbox>
         </div>
-        <div class="col-span-6">
+        <div class="col-span-12">
             <x-rapidez::checkbox v-model="variables.default_shipping">@lang('Default shipping address')</x-rapidez::checkbox>
         </div>
     </div>

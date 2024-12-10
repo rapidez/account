cf37196d9c7110660d17c30325ad80818f10ab0e
Jade Geels <jade@justbetter.nl>
2024-12-10 11:22:50 +0100

    Update check props

--- resources/views/account/address-edit.blade.php
+++ resources/views/account/address-edit.blade.php
@@ -7,7 +7,7 @@
 @section('account-content')
     <graphql
         query="{ customer { addresses { id firstname middlename lastname street city postcode country_code telephone company default_billing default_shipping } } }"
-        check="customer.addresses.find(a => a.id == {{ request()->id }})"
+        check="(data) => data.customer.addresses.find(a => a.id == {{ request()->id }})"
         redirect="{{ route('account.orders') }}"
     >
         <div v-if="data" slot-scope="{ data }">

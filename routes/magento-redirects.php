b68f48831c5261ed89027273f6fa1215c65af251
Jade Geels <jade@justbetter.nl>
2025-05-12 16:01:01 +0200

    Don't use routes because they're illegal

--- routes/magento-redirects.php
+++ routes/magento-redirects.php
@@ -7,21 +7,21 @@ Route::middleware('web')->group(function () {
     Route::get('customer/address/edit/id/{addressId}', function ($addressId) {
         return to_route('account.address', ['id' => $addressId], 301);
     });
-    Route::permanentRedirect('customer/address', route('account.addresses'));
-    Route::permanentRedirect('customer/address/index', route('account.addresses'));
-    Route::permanentRedirect('customer/address/new', route('account.address.create'));
-    Route::permanentRedirect('customer/account/create', route('account.register'));
-    Route::permanentRedirect('customer/account/edit', route('account.edit'));
-    Route::permanentRedirect('customer/account', route('account.overview'));
-    Route::permanentRedirect('customer/account/index', route('account.overview'));
-    Route::permanentRedirect('customer/account/login', route('account.login'));
-    Route::permanentRedirect('customer/account/forgotpassword', route('account.forgotpassword'));
+    Route::permanentRedirect('customer/address', '/account/addresses');
+    Route::permanentRedirect('customer/address/index', '/account/addresses');
+    Route::permanentRedirect('customer/address/new', '/account/address/new');
+    Route::permanentRedirect('customer/account/create', '/register');
+    Route::permanentRedirect('customer/account/edit', '/account/edit');
+    Route::permanentRedirect('customer/account', '/account');
+    Route::permanentRedirect('customer/account/index', '/account');
+    Route::permanentRedirect('customer/account/login', '/login');
+    Route::permanentRedirect('customer/account/forgotpassword', '/forgotpassword');
     Route::get('customer/account/createpassword', function (Request $request) {
         return redirect(str_replace('/customer/account/createpassword', '/resetpassword', $request->fullUrl()));
     });
 
     // Magento_Sales
-    Route::permanentRedirect('sales/order/history', route('account.orders'));
+    Route::permanentRedirect('sales/order/history', '/account/orders');
     Route::get('sales/order/view/order_id/{orderId}', function ($orderId) {
         return to_route('account.order', ['id' => $orderId], 301);
     });

a85faecf0699d8beb7a9e5c4e0663af295f66dd8
Tjeerd-171419 <tj.wokke@gmail.com>
2025-05-14 09:29:57 +0200

    removed if

--- resources/js/components/Account/PasswordStrength.vue
+++ resources/js/components/Account/PasswordStrength.vue
@@ -44,12 +44,10 @@ export default {
             this.errors = []
             this.strengths = []
             for (const check in this.checks) {
-                if (Object.prototype.hasOwnProperty.call(this.checks, check)) {
-                    if (new RegExp(this.checks[check].regex).test(this.password)) {
-                        this.strengths.push(this.checks[check].text)
-                    } else {
-                        this.errors.push(this.checks[check].text)
-                    }
+                if (new RegExp(this.checks[check].regex).test(this.password)) {
+                    this.strengths.push(this.checks[check].text)
+                } else {
+                    this.errors.push(this.checks[check].text)
                 }
             }
             let satisfiedClasses = Object.keys(this.checks).length - this.errors.length

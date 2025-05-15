8ec489a2237e158df8c03256f862a03b63bad1df
Tjeerd-171419 <tj.wokke@gmail.com>
2025-05-15 16:34:15 +0200

    Added password strength tile to edit account and orders page fix

--- resources/views/account/edit.blade.php
+++ resources/views/account/edit.blade.php
@@ -5,18 +5,18 @@
 @section('robots', 'NOINDEX,NOFOLLOW')
 
 @section('account-content')
-    <div class="grid grid-cols-1 gap-8 md:grid-cols-2 lg:grid-cols-3">
-        <div>
+    <div class="grid grid-cols-1 gap-8 lg:grid-cols-2">
+        <div class="col-span-2 md:col-span-1">
             <h2 class="mb-2 text-2xl font-bold">@lang('Account information')</h2>
             @include('rapidez::account.partials.edit.account')
         </div>
-        <div>
-            <h2 class="mb-2 text-2xl font-bold">@lang('Change password')</h2>
-            @include('rapidez::account.partials.edit.password')
-        </div>
-        <div>
+        <div class="col-span-2 md:col-span-1">
             <h2 class="mb-2 text-2xl font-bold">@lang('Change email')</h2>
             @include('rapidez::account.partials.edit.email')
+        </div>
+        <div class="col-span-2 md:col-span-1">
+            <h2 class="mb-2 text-2xl font-bold">@lang('Change password')</h2>
+            @include('rapidez::account.partials.edit.password')
         </div>
     </div>
 @endsection

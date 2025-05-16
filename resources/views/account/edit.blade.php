f95512b6c9831d4e83548a96ceed4c0190251634
Tjeerd-171419 <tj.wokke@gmail.com>
2025-05-16 13:03:09 +0200

    Styling feedback fix

--- resources/views/account/edit.blade.php
+++ resources/views/account/edit.blade.php
@@ -6,15 +6,15 @@
 
 @section('account-content')
     <div class="grid grid-cols-1 gap-8 lg:grid-cols-2">
-        <div class="col-span-2 md:col-span-1">
+        <div class="max-md:col-span-full">
             <h2 class="mb-2 text-2xl font-bold">@lang('Account information')</h2>
             @include('rapidez::account.partials.edit.account')
         </div>
-        <div class="col-span-2 md:col-span-1">
+        <div class="max-md:col-span-full">
             <h2 class="mb-2 text-2xl font-bold">@lang('Change email')</h2>
             @include('rapidez::account.partials.edit.email')
         </div>
-        <div class="col-span-2 md:col-span-1">
+        <div class="max-md:col-span-full">
             <h2 class="mb-2 text-2xl font-bold">@lang('Change password')</h2>
             @include('rapidez::account.partials.edit.password')
         </div>

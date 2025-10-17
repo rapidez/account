98740e60968fea45c3b6b663d4c3b8ac035cbb0b
Roene <roene@justbetter.nl>
2025-10-17 08:46:21 +0200

    Change padding back and break words with long email

--- resources/views/components/card/white.blade.php
+++ resources/views/components/card/white.blade.php
@@ -1,6 +1,6 @@
 <x-rapidez::tag
     is="{{ $tag ?? ($attributes->has('href') || $attributes->has('v-bind:href') ? 'a' : 'div') }}"
-    {{ $attributes->twMerge('bg-white flex gap-y-2 gap-x-6 rounded border px-7 py-4 text-left hover:opacity-80 max-sm:flex-col sm:items-center') }}
+    {{ $attributes->twMerge('bg-white flex gap-y-2 gap-x-6 rounded border px-8 py-4 text-left hover:opacity-80 max-sm:flex-col sm:items-center') }}
 >
     {{ $slot }}
 </x-rapidez::tag>
